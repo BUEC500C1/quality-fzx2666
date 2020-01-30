57b80762e5238315945226c1e3cf9e6f8bb069cc
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-01-29 20:40:14 -0400

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -5,4 +5,4 @@ def test_container():
     a = [1,100,1000,1500,2000,2500,3000]
     b = ['I','C','M','MD','MM','MMD','MMM']
     for x,y in zip(a,b):
-        assert Arab2Roman.intToRoman(x) == y
+        assert hw1.intToRoman(x) == y
