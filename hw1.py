42cbd2f84fde1ee0e6bf710b0d00818760c4cade
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-01-29 20:49:38 -0400

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,10 +1,10 @@
 
 def intToRoman(num):
-    hashmap = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+    dict = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
     s = ""
-    for k in hashmap:
+    for k in dict:
         if (num // k) != 0:
             count = num // k
             num %= k
-            s += hashmap[k] * count
+            s += dict[k] * count
     return s
