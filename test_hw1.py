41ce992187cb5bbc2b6cee8405dbf211baf2c4be
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-02-21 22:36:51 -0400

    Update test_hw1.py

--- test_hw1.py
+++ test_hw1.py
@@ -1,8 +1,16 @@
 import hw1
 import pytest
 
+print(hw1.intToRoman(4000))
+
 def test_container():
-    a = [1,100,1000,1500,2000,2500,3000]
-    b = ['I','C','M','MD','MM','MMD','MMM']
+    a = [1,100,1000,1500,2000,2500,3000,4000,35.6,'a','-1']
+    b = ['I','C','M','MD','MM','MMD','MMM','MMMM',-1,-1,-1]
     for x,y in zip(a,b):
         assert hw1.intToRoman(x) == y
+
+def main():
+    test_container()
+
+if __name__== "__main__":
+    main()
