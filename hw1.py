67aef1a83fff3ec54e278d53d657bb7a6abfd841
fzx2666 <46859529+fzx2666@users.noreply.github.com>
2020-02-21 22:37:16 -0400

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,7 +1,18 @@
-
 def intToRoman(num):
     dict = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
     s = ""
+    if type(num) is not int:
+        print("Please Input an Integer")
+        return -1
+
+    if type(num) is float:
+        print("Please Input an Integer Rather Than a Float")
+        return -1
+
+    if num<0:
+        print("Please Input a Number Bigger Than Zero")
+        return -1
+
     for k in dict:
         if (num // k) != 0:
             count = num // k
